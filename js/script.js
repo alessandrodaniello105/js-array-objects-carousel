711841ce02dce490198d60545c3a7065a344afc8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-22 13:39:09 +0200

    [del] arrayImgs strings; [add]: object imgs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
+// 2. Importo il blocco di codice dell'array di oggetti (immagini, titolo e testo slider)
 const arrayImgs = [
-  'img/01.webp',
-  'img/02.webp',
-  'img/03.webp',
-  'img/04.webp',
-  'img/05.webp'
+
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  },
+  
+  {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  },
+  
+  {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  },
+  
+  {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  },
+  
+  {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
+
 ];
 
 const itemsCollector = document.querySelector('.items-wrapper');
@@ -15,17 +42,31 @@ const btnNext = document.querySelector('.down');
 let counterImg = 0;
 
 
-for (let i = 0; i < arrayImgs.length; i++) {
+// for (let i = 0; i < arrayImgs.length; i++) {
 
-  let image = arrayImgs[i];
+//   let image = arrayImgs[i];
 
-  itemsCollector.innerHTML += `<img src="${image}" class="item">`;
-  thumbnail.innerHTML += `<img src="${image}" class="item-thumb">`;
+//   itemsCollector.innerHTML += `<img src="${image}" class="item">`;
+//   thumbnail.innerHTML += `<img src="${image}" class="item-thumb">`;
   
-  if (i === 0) document.querySelector('.item').classList.add('active');
-  if (i === 0) document.querySelector('.item-thumb').classList.add('active');
+//   if (i === 0) document.querySelector('.item').classList.add('active');
+//   if (i === 0) document.querySelector('.item-thumb').classList.add('active');
 
-};
+// };
+
+
+arrayImgs.forEach((immagine)=>{
+
+  itemsCollector.innerHTML += `<img src="${immagine.image}" class="item">`;
+
+  thumbnail.innerHTML += `<img src="${immagine.image}" class="item-thumb">`;
+
+  if (arrayImgs[0]) document.querySelector('.item').classList.add('active');
+
+  if (arrayImgs[0]) document.querySelector('.item-thumb').classList.add('active');
+  
+});
+
 
 const images = document.getElementsByClassName('item');
 const imagesThumbActive = document.getElementsByClassName('item-thumb');
